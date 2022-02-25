62669e48211f429cfe73576f02024f951a86b495
Yutthana00 <arf101293@gmail.com>
2022-02-25 11:41:45 +0000

    populated index.js with questions

--- util/index.js
+++ util/index.js
@@ -2,3 +2,27 @@
 const inquirer = require ("inquirer");
 const fs = require ('fs')
 const util = require ("util")
+
+// array of questions for README generation
+const questions = [{
+    type: "input",
+    message: "Enter project title",
+    Name: "",
+}, {
+    type: "input",
+    message: "Project discription",
+    Name: "",
+}, {
+    type: "input",
+    message: "License",
+    Name: "",
+}, {
+    type: "input",
+    message: "GitHub user name",
+    Name: "",
+}, {
+    type: "input",
+    message: "Email address",
+    Name: "",
+}
+]
