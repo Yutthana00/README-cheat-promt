198e38a65d66f9730545f32e84725a95647cacd3
Yutthana00 <arf101293@gmail.com>
2022-02-26 20:45:11 +0000

    license badge in new location, layout adjustment

--- READMEgenerator/fileGenerator.js
+++ READMEgenerator/fileGenerator.js
@@ -4,49 +4,84 @@ function fileGenerator(response) {
   
   ${response.title}
   
-  #### Description:
+  <br>
+
+  ![Github](https://img.shields.io/github/license/${response.userName}/${response.title})
+
+  <br>
+
+  **Description**:
+  <br>
   ${response.description}
   
+  <br>
+
+  **Github-Repositories**:
+  <br>
+  https://github.com/${response.userName}/${response.title}
+  
+  <br>
+  
+ 
+  **License**:
+  <br>
+  This project is licensed under the ${response.license} license.
+
+  <br>
+
+  ![Github](https://img.shields.io/github/license/${response.userName}/${response.title})
+  
+  <br>
+
   ## Table of contents
+  * [Github Repository](#Github-Repositories)
   * [Installation](#Installation)
   * [Usage](#Usage)
-  * [Contribution](#Contribution)
+  * [Contributor](#Contribution)
   * [Tests](#Test)
   * [License](#License)
-  * [User-Name](#UserName)
+  * [Github User Name](#UserName)
   * [Email](#Email)
   
-  ###### [Back to Table of Contents](#Table-of-Contents)
+  <br>
   
-  ###**Installation**:
+  **Installation**:
+  <br>
   ${response.installation} 
   
+  <br>
   
-  ###**Usage**:
+  **Usage**:
+  <br>
   ${response.usage}
 
+  <br>
  
-  ###**Contribution** :
-  ${response.contribution}
+  **Contributor**:
+  <br>
+  This project is made by: ${response.contribution}
  
-  
-  ### **Tests** :
-  ${response.test}
-  
+  <br>
 
+  **Tests**:
+  <br>
+  For testing, please enter **${response.test}** into your terminal.
   
-  ###**userName**:
-  ${response.userName} 
+  <br>
+
+  **Github User Name**:
+  <br>
+  ${response.userName}
+
+  <br>
   
+  **Email**:
+  <br>
+  If you have further questions about my project, please reach out to me at:  ${response.email} 
   
-  ###**email**:
-  ${response.email}
-  
- 
-  ###**License**:
-  ${response.license}
-  
-  ######[Back to Table of Contents](#table)
+  <br>
+
+  ###### [Back to Table of Contents](#Table-of-Contents)
   
   `
   return content; 
